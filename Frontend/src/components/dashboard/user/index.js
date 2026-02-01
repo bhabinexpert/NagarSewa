1eb7f71009b6abcff4cb7efae417485f6374c742
unknown <bhabindulal35@gmail.com>
2026-02-01 21:41:17 +0545

    refactor: remove Notifications from component exports

--- Frontend/src/components/dashboard/user/index.js
+++ Frontend/src/components/dashboard/user/index.js
@@ -2,6 +2,5 @@
 export { default as ReportIssue } from './ReportIssue';
 export { default as UserProfile } from './UserProfile';
 export { default as IssueHistory } from './IssueHistory';
-export { default as Notifications } from './Notifications';
 export { default as NewsFeed } from './NewsFeed';
 export { default as RequestCampaign } from './RequestCampaign';
