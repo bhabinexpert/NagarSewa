00397a4ebd0a385da3934f9450672517187d1428
unknown <bhabindulal35@gmail.com>
2026-02-03 21:42:53 +0545

    feat: export MyHistory component in user dashboard index
    - Add MyHistory component export for user issue and campaign history tracking

--- Frontend/src/components/dashboard/user/index.js
+++ Frontend/src/components/dashboard/user/index.js
@@ -2,5 +2,6 @@
 export { default as ReportIssue } from './ReportIssue';
 export { default as UserProfile } from './UserProfile';
 export { default as IssueHistory } from './IssueHistory';
+export { default as MyHistory } from './MyHistory';
 export { default as NewsFeed } from './NewsFeed';
 export { default as RequestCampaign } from './RequestCampaign';
