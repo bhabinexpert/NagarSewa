bc9d755ca54f158576ed37a3ef4596a27fb1a1fc
bhabinexpert <bhabindulal35@gmail.com>
2026-03-24 14:32:33 +0545

    database hosted

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -8,11 +8,15 @@ const { Pool } = pg;
 
 // Create connection pool
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432,
+  // user: process.env.PG_USER,
+  // host: process.env.PG_HOST,
+  // database: process.env.PG_DATABASE,
+  // password: process.env.PG_PASSWORD,
+  // port: process.env.PG_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl:{
+    rejectUnauthorized: false,
+  }
 });
 
 // Test database connection
